ad6c523084882a678bb0d1b968b3db0cb6d3f308
Md Imam Ahasan <emamahasane@gmail.com>
2022-12-01 06:45:44 +0600

    Add files via upload

---
+++ state/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import State from './component/State';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="headerStyle">Welcome To React</h1>
+      <State />
+    </div>
+  );
+}
+
+export default App;
---
+++ state/src/component/State.js
@@ -0,0 +1,39 @@
+import { click } from '@testing-library/user-event/dist/click';
+import React, { Component } from 'react';
+
+class State extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  handleIncrement = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+
+  handleDecrement = () => {
+    this.setState({
+      count: this.state.count - 1,
+    });
+  };
+
+  render() {
+    const { count } = this.state;
+    return (
+      <div>
+        <h1>Count: {count}</h1>
+        <button onClick={this.handleIncrement}>+</button>
+
+        <button onClick={this.handleDecrement} disabled={count === 0 ? true : false }>
+          -
+        </button>
+      </div>
+    );
+  }
+}
+
+export default State;
