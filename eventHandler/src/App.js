0cf17336282dc9df1723f287550d157c0583d017
Md Imam Ahasan <emamahasane@gmail.com>
2022-12-12 00:25:28 +0600

    Add files via upload

---
+++ eventHandler/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import EvnHandCls from './component/eventHandlerClass/eventHandlerClass';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome To Our ReactJS Project</h1>
+
+      <EvnHandCls />
+    </div>
+  );
+}
+
+export default App;
---
+++ eventHandler/src/component/eventHandlerClass/eventHandlerClass.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class EvnHandCls extends Component {
+  constructor(props) {
+    super(props) 
+
+    this.state = {
+      changedValue: ''
+    }
+  }
+
+  handleChange = (e) => {
+
+    this.setState({
+      changedValue: e.target.value
+    }, () => {
+      console.log(this.state.changedValue)
+    })
+    
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text" onChange={this.handleChange} />
+        <p>{this.state.changedValue}</p>
+      </div>
+    )
+  }
+}
+
+export default EvnHandCls;
