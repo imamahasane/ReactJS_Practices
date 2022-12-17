68598639b5f5631289c38da14b34b4804575e6e5
Md Imam Ahasan <emamahasane@gmail.com>
2022-12-18 03:59:52 +0600

    Add files via upload

---
+++ useEffect/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import UseEffect from './component/UseEffect/UseEffect';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="proHead">Welcome To My ReactJS Project</h1>
+
+      <UseEffect />
+    </div>
+  );
+}
+
+export default App;
