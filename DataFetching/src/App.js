4b1a762fcf477f401322e19c09ec253205ff110e
Md Imam Ahasan <emamahasane@gmail.com>
2022-12-19 23:37:22 +0600

    Add files via upload

---
+++ DataFetching/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import DataFetch from './component/UseEffect/DataFetch';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="proHead">Welcome To My ReactJS Project</h1>
+
+      <DataFetch />
+    </div>
+  );
+}
+
+export default App;
